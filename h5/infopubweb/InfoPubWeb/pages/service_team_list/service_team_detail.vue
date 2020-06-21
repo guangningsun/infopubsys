92e66c9021789e7bdfc66d41b96fab36315e9274
Victor Mak <victormak357@gmail.com>
2020-06-21 18:21:55 +0800

    微信jssdk

--- h5/infopubweb/InfoPubWeb/pages/service_team_list/service_team_detail.vue
+++ h5/infopubweb/InfoPubWeb/pages/service_team_list/service_team_detail.vue
@@ -248,7 +248,7 @@ export default {
 			  longitude: parseFloat(item.longitude), // 经度，浮点数，范围为180 ~ -180。
 			  name: item.name, // 位置名
 			  address: item.address, // 地址详情说明
-			  scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+			  scale: 10, // 地图缩放级别,整形值,范围从1~28。默认为最大
 			  infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
 			});
 		},
