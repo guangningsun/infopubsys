b0c19d37141a22721ee3d6f6c4baf5ff4860fe6d
Victor Mak <victormak357@gmail.com>
2020-06-21 22:17:09 +0800

    生产环境，去掉debug

--- h5/infopubweb/InfoPubWeb/pages/service_team_list/service_team_detail.vue
+++ h5/infopubweb/InfoPubWeb/pages/service_team_list/service_team_detail.vue
@@ -186,11 +186,12 @@ export default {
 			console.log(this.js_sdk_info);
 			var jweixin = require('jweixin-module');
 			jweixin.config({
-			  debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+			  debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 			  appId: 'wxed4a279d2cdba74e', // 必填，公众号的唯一标识
 			  timestamp: this.js_sdk_info.timestamp, // 必填，生成签名的时间戳
 			  nonceStr: this.js_sdk_info.nonceStr, // 必填，生成签名的随机串
 			  signature: this.js_sdk_info.signature,// 必填，签名
+			  scale: 22, // 地图缩放级别,整形值,范围从1~28。默认为最大
 			  jsApiList: ['openLocation','getLocation'] // 必填，需要使用的JS接口列表
 			});
 			
