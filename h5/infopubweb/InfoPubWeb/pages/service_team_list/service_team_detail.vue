1c07cbe0c51e4d2c17bee5345dceb8337e906244
Victor Mak <victormak357@gmail.com>
2020-06-21 17:58:50 +0800

    微信jssdk

--- h5/infopubweb/InfoPubWeb/pages/service_team_list/service_team_detail.vue
+++ h5/infopubweb/InfoPubWeb/pages/service_team_list/service_team_detail.vue
@@ -187,7 +187,7 @@ export default {
 			var jweixin = require('jweixin-module');
 			jweixin.config({
 			  debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-			  appId: 'wxed4a279d2cdba74e', // 必填，公众号的唯一标识
+			  appId: 'wx84243bcfe85ee507', // 必填，公众号的唯一标识
 			  timestamp: this.js_sdk_info.timestamp, // 必填，生成签名的时间戳
 			  nonceStr: this.js_sdk_info.nonceStr, // 必填，生成签名的随机串
 			  signature: this.js_sdk_info.signature,// 必填，签名
